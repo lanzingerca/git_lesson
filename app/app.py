7e0de81828c44a4b16ca130683725582b19d35bb
Don Lanzinger <d.lanzinger@verizon.net>
2017-07-27 15:31:57 -0700

    Added Python script, for pushing to GitHub

---
+++ app/app.py
@@ -0,0 +1,15 @@
+'''Python script that prints out'''
+
+
+class ClassName(object):
+	"""Generic classname"""
+	def __init__(self, arg):
+		self.arg = arg
+
+	def method(self):
+		a = 10
+		for i in range(5):
+			print(a + i)
+
+c = ClassName(55)
+print(c.method())
